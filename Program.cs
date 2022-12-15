175b1f2a8218c3f9e359c2a9cbfcc7a3ab6b088c
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-15 12:08:47 +0300

    Zarabotalo!

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Номер дня недель вводи, Карапузейро!) число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{ Console.WriteLine("Ты ввел Понедельник, Бамбуча ты сочная..."); }
+else if (a == 2)
+{ Console.WriteLine("Ты же ввел Вторник, хитрец ты этакий!"); }
+else
+{ Console.WriteLine("Остальные дни я еще не знаю как называются на твоем языке, Варвар!"); }
