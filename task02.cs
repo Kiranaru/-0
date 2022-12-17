286e81ebe2fd0b7be0080143328794ddfc052347
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-17 22:19:41 +0300

    Add task02 as my 1'st home work

---
+++ task02.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, котоая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+Console.WriteLine("**********************************************");
+Console.WriteLine("****** Введите Первое Натуральное Число ******");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("**********************************************");
+Console.WriteLine("****** Введите Второе Натуральное Число ******");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b)
+{
+    Console.WriteLine("****************** EQUALITY ******************");
+}
+else if (a > b)
+{
+    Console.WriteLine($"The first number {a} is GREATER then the second one {b}");
+}
+else
+{
+    Console.WriteLine($"The first number {a} is LESS then the second one {b}");
+}
