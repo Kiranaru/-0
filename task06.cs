0e0b5034471f6a1a82d35bd82c04ba8b977f8134
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-18 22:59:39 +0300

    Yes, that's my third one!

---
+++ task06.cs
@@ -0,0 +1,10 @@
+﻿// Write a program that takes a number as input and outputs whether it is even
+Console.WriteLine("Please, do not hasitate to input any Integer Number, Sir!");
+int Number = Convert.ToInt32(Console.ReadLine());
+int DivNum = Number % 2;
+if (DivNum == 0)
+{ Console.WriteLine($"Yes, Sir! Your Numer {Number} is quite EVEN!"); }
+else
+{
+    { Console.WriteLine($"Sorry, Sir! But Your Numer {Number} is a little bit differ from EVEN!"); }
+}
