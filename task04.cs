d1ef25aa0699136bc797283531b966d0b5327d74
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-18 22:38:05 +0300

    Add files via upload

---
+++ task04.cs
@@ -0,0 +1,17 @@
+﻿// Write a program that takes three numbers as input and outputs the maximum of them
+Console.WriteLine("**********************************************");
+Console.WriteLine("the First Number input here, please");
+int FirstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("the Second Number input here, please");
+int SecondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("the Third Number input here, please");
+int ThirdNum = Convert.ToInt32(Console.ReadLine());
+int MaxNum = FirstNum;
+if (SecondNum>ThirdNum)
+{MaxNum=SecondNum;
+if (FirstNum>SecondNum)
+{MaxNum=FirstNum;}}
+else
+{if (ThirdNum>FirstNum)
+{MaxNum=ThirdNum;}}
+Console.WriteLine($" Maximum number is {MaxNum}");
