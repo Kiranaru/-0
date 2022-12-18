58e117b8e86bc456677de916167668ab8af5bbd8
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-18 22:42:57 +0300

    Add once more this file after Formatting

--- task04.cs
+++ task04.cs
@@ -7,11 +7,15 @@ int SecondNum = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("the Third Number input here, please");
 int ThirdNum = Convert.ToInt32(Console.ReadLine());
 int MaxNum = FirstNum;
-if (SecondNum>ThirdNum)
-{MaxNum=SecondNum;
-if (FirstNum>SecondNum)
-{MaxNum=FirstNum;}}
+if (SecondNum > ThirdNum)
+{
+    MaxNum = SecondNum;
+    if (FirstNum > SecondNum)
+    { MaxNum = FirstNum; }
+}
 else
-{if (ThirdNum>FirstNum)
-{MaxNum=ThirdNum;}}
+{
+    if (ThirdNum > FirstNum)
+    { MaxNum = ThirdNum; }
+}
 Console.WriteLine($" Maximum number is {MaxNum}");
