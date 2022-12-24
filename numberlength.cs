46d2489cc7d58474a8cb189068379688ae62f402
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-24 15:05:11 +0300

    Number's (posotive, less then 10 digits) length function.

---
+++ numberlength.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Please, input positive integer number (less than 9 millions)");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The length of your number is {NumLength(number)} digits!");
+
+
+int NumLength(int numero)
+{
+    int index = 0;
+    int numozoid = 0;
+    int tens = 1;
+    while (numero > numozoid)
+    {
+        index++;
+        numozoid = numozoid + ((numero - numozoid) / tens % 10) * (tens);
+        tens = tens * 10;
+    }
+    return index;
+}
