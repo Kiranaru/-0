129ce2f598ce5866c08cce45782436fd915ea7cc
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-24 16:29:48 +0300

    Check int number - wether it polyndromic or not

---
+++ task19.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Please, input integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] num = new int[32];
+int marker = 0;
+
+int halflength = NumDataLength(number) / 2;
+int length = NumDataLength(number);
+Console.WriteLine($"length: {NumDataLength(number)}");
+Console.WriteLine($"halflength: {halflength}");
+int ind = 0;
+while (ind < halflength)
+{
+    ind++;
+    if (num[ind] != num[length - ind + 1]) marker++;
+}
+if (marker == 0) Console.WriteLine("Your number is POLYNDROMIC !");
+else Console.WriteLine("Sorry, but your number is casual...");
+
+int NumDataLength(int numero)
+{
+    int index = 0;
+    int numozoid = 0;
+    int tens = 1;
+    while (numero > numozoid)
+    {
+        index++;
+        num[index] = (number - numozoid) / tens % 10;
+        numozoid = numozoid + num[index] * (tens);
+        tens = tens * 10;
+    }
+    return index;
+}
