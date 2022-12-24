01c66b545db4d8df637c32b65d5fb202df46c817
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-24 15:29:38 +0300

    Task 10 HomeWork

---
+++ task10.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please, input three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999 || number < 100)
+{
+    Console.WriteLine("Your number is not a three-digit number , Buddy! Try again");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Yeah! On position 2 of your number is value: {Num2Digit(number)}");
+
+int Num2Digit(int numero)
+{
+    int num = ((numero - (numero % 10)) / 10) % 10;
+    return num;
+}
