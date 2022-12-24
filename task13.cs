3769b151ed06d9a5617e7af5a0fc2532843a9223
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-24 15:42:51 +0300

    Task 13 HomeWork

---
+++ task13.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please, input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100) Console.WriteLine("Your number hasn't got any third number!");
+else{
+Console.WriteLine($"Yeah! On position 3 of your number is value: {Num3Digit(number)}");
+}
+
+int Num3Digit(int numero)
+{
+    int num = ((numero-((((numero-(numero%10))/10)%10)*10-numero%10))/100)% 10 ;
+    return num;
+}
