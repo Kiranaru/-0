13a5c11bf7f26931825c5a76e8257833fd5b2b44
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-24 15:54:54 +0300

    Task 15 HomeWork

---
+++ task15.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please, input Week-day number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1 || number > 7)
+{
+    Console.WriteLine("Your number doesn't fit to Week-number, try again.");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+if (number < 6) Console.WriteLine("Wow - this day is Working day!");
+else Console.WriteLine("Yeah! Today is Holyday!");
