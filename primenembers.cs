b16e86347f4a2c6e20e30ecae80b732d0d7febe1
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-25 19:17:59 +0300

    Is your number prime - this function can check it.

---
+++ primenembers.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Input NUMBER you would like to know whether it is prime or not.");
+int number = Convert.ToInt32(Console.ReadLine());
+int indexPrimeNumber = 1;
+bool markerPrimeNumber = true;
+
+if (PrimeNumber(number)) Console.WriteLine($"YES, Your number {number} is PRIME!\n");
+else
+{
+    Console.WriteLine($"Sorry, but Your number {number} is NOT prime...\n");
+}
+
+
+bool PrimeNumber(int numPrimeNumber)
+{
+    indexPrimeNumber = 1;
+    markerPrimeNumber = true;
+    while (markerPrimeNumber && indexPrimeNumber < numPrimeNumber - 1)
+    {
+        indexPrimeNumber++;
+        if (numPrimeNumber % indexPrimeNumber == 0) markerPrimeNumber = false;
+    }
+            return markerPrimeNumber;    
+}
