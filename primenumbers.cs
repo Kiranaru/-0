3fdf38d858c753e063b417a73df70354bdf71b94
Kirill Khvostunkov <65184247+Kiranaru@users.noreply.github.com>
2022-12-27 12:54:16 +0300

    I can find Prime Numbers in every region

---
+++ primenumbers.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+Console.WriteLine("Input Left and right numbers for range you would like to find prime numbers in.\n");
+Console.WriteLine("Input number for left range. Greater than 1");
+int leftrange = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number for left range. Greater than previous number.");
+int rightrange = Convert.ToInt32(Console.ReadLine());
+while (rightrange < leftrange && leftrange < 1)
+{
+    Console.WriteLine("Your Left number must be less then right one.\n");
+    Console.WriteLine("Input Left and right numbers for range you would like to find prime numbers in.\n");
+    leftrange = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Input Left and right numbers for range you would like to find prime numbers in.\n");
+    rightrange = Convert.ToInt32(Console.ReadLine());
+}
+int[] primenum = new int[rightrange];
+int indexPrimeNumber = 1;
+bool markerPrimeNumber = true;
+int counter = -1;
+int index = leftrange;
+while (index < rightrange)
+{
+    if (PrimeNumber(index, leftrange))
+    {
+        counter++;
+        primenum[counter] = index;
+        Console.Write($"{(char)16} {index}  ,");        
+    }
+    index++;
+}
+Console.WriteLine(primenum.Length);        
+
+bool PrimeNumber(int numPrimeNumber, int leftPrimeNumber)
+{
+    indexPrimeNumber = 1;
+    markerPrimeNumber = true;
+    while (markerPrimeNumber && indexPrimeNumber < numPrimeNumber-1 )
+    {
+        indexPrimeNumber++;
+        if (numPrimeNumber % indexPrimeNumber == 0) markerPrimeNumber = false;
+    }
+    return markerPrimeNumber;
+}
+
